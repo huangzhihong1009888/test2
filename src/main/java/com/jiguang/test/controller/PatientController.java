599387a852645bce0202411f372570ad55b7324e
huang <504626667@qq.com>
2026-09-22 15:45:02 +0800

    测试

---
+++ src/main/java/com/jiguang/test/controller/PatientController.java
@@ -0,0 +1,26 @@
+package com.jiguang.test.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author duguohui
+ */
+@Api(tags = "患者档案信息接口")
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+
+
+    @ApiOperation("查询患者信息（医保专用）")
+    @PostMapping("/getPatientById")
+    public String getByYb(){
+        return "success";
+    }
+
+
+}
