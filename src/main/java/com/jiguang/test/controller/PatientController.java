08b80e6ec4c313e4bab121d16c0f91ced34d757d
huang <504626667@qq.com>
2026-09-22 17:22:24 +0800

    测试

--- src/main/java/com/jiguang/test/controller/PatientController.java
+++ src/main/java/com/jiguang/test/controller/PatientController.java
@@ -19,7 +19,7 @@ public class PatientController {
     @ApiOperation("查询患者信息（医保专用）")
     @PostMapping("/getPatientById")
     public String getByYb(){
-        return "success";
+        return "恭喜你查询到患者信息（医保专用）";
     }
 
 
