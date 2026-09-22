ab8e13df8b3453194800c70cb11cea13d59c89e5
huang <504626667@qq.com>
2026-09-22 14:17:49 +0800

    删除

--- src/main/java/com/jiguang/test/config/datasource/DataSourceConfig.java
+++ src/main/java/com/jiguang/test/config/datasource/DataSourceConfig.java
@@ -3,7 +3,6 @@ package com.jiguang.test.config.datasource;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
@@ -13,18 +12,13 @@ import javax.sql.DataSource;
  **/
 @Configuration
 public class DataSourceConfig {
+
     @Bean
-    @Primary
-    public DataSource dynamicDataSource() {
-        DynamicDataSource dataSource = new DynamicDataSource();
-        dataSource.setDefaultTargetDataSource(getDataSource1());
-        return dataSource;
-    }
-    public DataSource getDataSource1() {
+    public DataSource getDataSource() {
         DruidDataSource source = new DruidDataSource();
         source.setPassword("root");
         source.setUsername("root");
-        source.setUrl("jdbc:mysql://182.92.188.87:3306/test?useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true&serverTimezone=GMT%2B8");
+        source.setUrl("jdbc:mysql://localhost:3306/yoga?useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true&serverTimezone=GMT%2B8");
         source.setDriverClassName("com.mysql.cj.jdbc.Driver");
         return source;
     }
--- src/main/java/com/jiguang/test/enums/IEnum.java
+++
@@ -1,7 +0,0 @@
-package com.jiguang.test.enums;
-
-public interface IEnum<T,D> {
-    D getValue();
-    T getType();
-
-}
